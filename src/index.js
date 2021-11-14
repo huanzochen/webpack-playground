b25edb328288c9478c1371a21d8cd590bf7ae12d
tzuchuan.huang <tzuchuan.huang@rakuten.com>
2021-11-14 11:01:54 +0800

    add nine

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import "./main.scss";
 import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 const alertService = new AlertService();
-const componentService = new ComponentService();
+const componentService = new ComponentService()
 run(alertService, componentService);
--- webpack.common.js
+++ webpack.common.js
@@ -17,6 +17,22 @@ module.exports = {
           "css-loader", //2. Turns css into commonjs
           "sass-loader" //1. Turns sass into css
         ]
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: "imgs"
+            }
+          }
+        ]
       }
     ]
   }
