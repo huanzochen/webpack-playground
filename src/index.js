2edb35309d48a03f5942b16865b9ad2e2cb010b0
tzuchuan.huang <tzuchuan.huang@rakuten.com>
2021-11-14 11:16:22 +0800

    add nine

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 const alertService = new AlertService();
 const componentService = new ComponentService()
-run(alertService, componentService);
+run(alertService, componentService)
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,15 @@
 const path = require("path");
 const merge = require('webpack-merge')
 const common = require('./webpack.common')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = merge(common, {
-  mode: "development",
+  mode: "production",
   output: {
     filename: "main.[contentHash].js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ]
 });
