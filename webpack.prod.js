8599019a166af7fe7d89ec5fcdd9afa9b3dd0773
tzuchuan.huang <tzuchuan.huang@rakuten.com>
2021-11-14 10:03:03 +0800

    eightme

---
+++ webpack.prod.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
+
+module.exports = merge(common, {
+  mode: "development",
+  output: {
+    filename: "main.[contentHash].js",
+    path: path.resolve(__dirname, "dist")
+  }
+});
